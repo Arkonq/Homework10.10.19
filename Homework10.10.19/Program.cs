a1b53d44626bf8c99d933ac6a32e5f25acb4a8a4
arkonq@gmail.com <arkonq@gmail.com>
2019-10-12 15:04:02 +0600

    Добавьте файлы проекта.

---
+++ Homework10.10.19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Homework10._10._19
+{
+	class Program
+	{
+		/*
+		Напишите код с использованием технологии ADO.NET, который создает в базе данных таблицу gruppa. (Id,Name) 
+		*/
+		static void CreateTable()
+		{
+			Console.WriteLine("Введите название сервера:");
+			string serverName = Console.ReadLine();
+			Console.WriteLine("Введите название базы данных:");
+			string dataBaseName = Console.ReadLine();
+			string connectionString = $"Server={serverName};Database={dataBaseName};Trusted_Connection=True;";
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			using (SqlCommand sqlCommand = connection.CreateCommand())
+			{
+				string query = $"CREATE TABLE gruppa (" +
+													$"Id uniqueidentifier," +
+													$"Name nvarchar(50)" +
+													$");";
+				sqlCommand.CommandText = query;
+
+				connection.Open();
+				sqlCommand.ExecuteNonQuery();
+			}
+		}
+		static void Main(string[] args)
+		{
+			try
+			{
+				CreateTable();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.Message);
+			}
+			Console.WriteLine("End Of Program!");
+			Console.Read();
+		}
+	}
+}
